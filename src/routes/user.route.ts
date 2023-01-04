66a1c39e184d89618a2cc31c8d326507237b8190
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-01-05 01:37:30 +0900

    user route 예제 코드 추가

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -8,4 +8,11 @@ import { UserService } from 'src/services/index.service';
 
 const router = Router();
 
+router.get( '/users/:userUuid', responseWrapper( async ( req: Request, res: Response ) => {
+  const { userUuid } = req.params;
+  
+  console.log( userUuid );
+  resSuccess( res, { result: userUuid });
+}) );
+
 export default router;

6db69e4582fe3829008641a17cfa6dd49cd4a86a
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-01-11 03:29:32 +0900

    fix: joinValidation 세분화

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -5,18 +5,21 @@ import { removeEmptySpace, isEmail } from '../utils/stringUtil';
 import { generateHashPassword } from '../utils/hash';
 
 export const joinValidation = ( req, res, next ) => {
+  const { userId, password, email } = req.body;
+
+  if ( !userId.trim() || !password.trim() || !email.trim() ) {
+    throw new ErrorException( badData );
+  }
+  if ([ userId, password, email ].some( elem => typeof elem !== 'string' ) || !isEmail( email ) ) {
+    throw new ErrorException( badData );
+  }
+
   Object.keys( req.body ).forEach( key => {
     if ( key !== 'userId' && key !== 'password' && key !== 'email' ) {
       return;
     }
     req.body[key] = removeEmptySpace( req.body[key]);
   });
-
-  const { userId, password, email } = req.body;
-
-  if ( !userId || !password || !email || !isEmail( email ) ) {
-    throw new ErrorException( badData );
-  }
 
   next();
 };

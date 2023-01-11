d8e5eec93fdc0dcc53a675f9c4e347baf486aab0
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-01-12 02:00:49 +0900

    add: joinValidation 함수 주석 추가

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -18,16 +18,19 @@ const removeAllBlank = ( obj: Record<string, string> ): Record<string, string> =
 export const joinValidation = ( req: Request, res: Response, next: NextFunction ) => {
   const { userId, password, email } = req.body;
 
+  // type 및 값 유무 체크
   [ userId, password, email ].forEach( elem => {
     if ( typeof elem !== 'string' || !elem.trim() ) {
       throw new ErrorException( badData );
     }
   });
 
+  // email 형식 체크
   if ( !isEmail( email ) ) {
     throw new ErrorException( badData );
   }
 
+  // 띄어쓰기 제거
   req.body = removeAllBlank({ userId, password, email });
 
   next();

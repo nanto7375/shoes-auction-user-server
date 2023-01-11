26b6980727e53493ddd7b08f7bb50595a1c1057f
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-01-12 01:58:15 +0900

    middleware req destruct 취소

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -2,21 +2,21 @@ import { Request, Response, NextFunction } from 'express';
 import ErrorException from '../exceptions/form.exception';
 import { badData } from '../exceptions/definition.exception';
 
-import { removeEmptySpace, isEmail } from '../utils/stringUtil';
+import { removeBlank, isEmail } from '../utils/stringUtil';
 import { generateHashPassword } from '../utils/hash';
 
-const removeAllEmptySpace = ( obj: Record<string, string> ): Record<string, string> => {
+const removeAllBlank = ( obj: Record<string, string> ): Record<string, string> => {
   const copiedObj = { ...obj };
 
   Object.keys( copiedObj ).forEach( key => {
-    copiedObj[key] = removeEmptySpace( copiedObj[key]);
+    copiedObj[key] = removeBlank( copiedObj[key]);
   });
 
   return copiedObj;
 };
 
-export const joinValidation = ({ body }: Request, res: Response, next: NextFunction ) => {
-  const { userId, password, email } = body;
+export const joinValidation = ( req: Request, res: Response, next: NextFunction ) => {
+  const { userId, password, email } = req.body;
 
   [ userId, password, email ].forEach( elem => {
     if ( typeof elem !== 'string' || !elem.trim() ) {
@@ -28,16 +28,16 @@ export const joinValidation = ({ body }: Request, res: Response, next: NextFunct
     throw new ErrorException( badData );
   }
 
-  body = removeAllEmptySpace({ userId, password, email });
+  req.body = removeAllBlank({ userId, password, email });
 
   next();
 };
 
-export const hashPassword = ({ body }: Request, res: Response, next: NextFunction ) => {
-  const { password } = body;
+export const hashPassword = ( req: Request, res: Response, next: NextFunction ) => {
+  const { password } = req.body;
 
-  body.hashedPassword = generateHashPassword( password );
-  delete body.password;
+  req.body.hashedPassword = generateHashPassword( password );
+  delete req.body.password;
 
   next();
 };
--- src/utils/stringUtil.ts
+++ src/utils/stringUtil.ts
@@ -1,4 +1,4 @@
-export const removeEmptySpace = ( str = '' ): string => str.replace( / /g, "" );
+export const removeBlank = ( str = '' ): string => str.replace( / /g, "" );
 
 export const isEmail = ( email: string ): boolean => {
   const regexEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;

66b3efc24fb207ff23beabd286b443f0c953b0d5
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-01-12 02:05:29 +0900

    refactor: register parameter destructure 제거

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -3,13 +3,13 @@ import db from '../models';
 
 const { User } = db;
 
-export const register = async ({ userId, hashedPassword, email }) => {
-  const user = await User.create({ userId, hashedPassword, email });
+export const register = async ( body: Record<string, unknown> ) => {
+  const user = await User.create( body );
 
   return user;
 };
 
-export const findUserByUuid = async ( uuid ) => {
+export const findUserByUuid = async ( uuid: string ) => {
   const user = await User.findOne({ where: { uuid }, attributes: [ 'email' ] });
 
   return user;

d18ac09a7ddda9d2a9963360848b7ec7651ec44c
bigboyang <87364146+bigboyang@users.noreply.github.com>
2023-01-16 22:20:52 +0900

    modify join API parameter & create find-password API

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 // import { Joi, Segments, celebrate } from 'celebrate';
 
 import ErrorException from '../exceptions/form.exception';
-import { badData } from '../exceptions/definition.exception';
+import { badData, badRequest } from '../exceptions/definition.exception';
 import { resSuccess, responseWrapper } from '../utils/handler';
 
 import { UserService } from '../services';
@@ -13,9 +13,9 @@ const router = Router();
 /**회원가입
  * req.body type { userId: string; hashedPassword: string; email: string; }*/
 router.post( '/users', UserMiddleware.joinValidation, UserMiddleware.hashPassword, responseWrapper( async ( req: Request, res: Response ) => {
-  const { userId, hashedPassword, email } = req.body;
+  const { userId, password, email } = req.body;
 
-  const user = await UserService.register({ userId, hashedPassword, email });
+  const user = await UserService.register({ userId, password, email });
   
   resSuccess( res, { result: user });
 }) );
@@ -30,6 +30,29 @@ router.get( '/users/:userUuid', responseWrapper( async ( req: Request, res: Resp
 
   console.log( userUuid );
   resSuccess( res, { result: userUuid });
+}) );
+
+/**
+ * 비밀번호 찾기 - 임시비밀번호 발급 
+ * req.query ?userId=uuid&email=email
+*/
+router.get( '/find-password' ,responseWrapper( async ( req: Request, res: Response ) => {
+  const{ userId, email }  = req.query;
+
+  if ( !userId || !email ) {
+    throw new ErrorException( badData );
+  }
+
+  const isExistUser = await UserService.isExistUser( userId as string, email as string );
+
+  if ( !isExistUser ) {
+    throw new ErrorException( badRequest );
+  }
+
+  const tempPassword = Array( 10 ).fill( null ).map( () => Math.round( Math.random() * 10 ) ).join( '' ); 
+  await UserService.updatePassword({ userId, email, tempPassword });
+  
+  resSuccess( res, { tempPassword : tempPassword });
 }) );
 
 export default router;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,20 @@
 import { UserRepository } from '../repositories';
 
-export const register = async ({ userId, hashedPassword, email }) => {
-  const user = await UserRepository.register({ userId, hashedPassword, email });
+export const register = async ({ userId, password, email }) => {
+  const user = await UserRepository.register({ userId, password, email });
+
+  return user;
+};
+
+export const isExistUser = async ( userId: string  , email: string  ): Promise<boolean> => {
+
+  const userUuid = await UserRepository.isExistUser( userId, email );
+  
+  return !!userUuid;
+};
+
+export const updatePassword = async ({ userId, email, tempPassword }) => {
+  const user = await UserRepository.updatePassword({ tempPassword }, { where: { userId, email } });
 
   return user;
 };

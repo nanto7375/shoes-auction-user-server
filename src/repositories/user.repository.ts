82db426f2e07c303cc1cd31dd6717bb323e5a3c6
bigboyang <87364146+bigboyang@users.noreply.github.com>
2023-01-16 21:57:52 +0900

    find-password & modify join-parameter

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -3,8 +3,8 @@ import db from '../models';
 
 const { User } = db;
 
-export const register = async ({ userId, hashedPassword, email }) => {
-  const user = await User.create({ userId, hashedPassword, email });
+export const register = async ( body: Record<string, unknown> ) => {
+  const user = await User.create( body );
 
   return user;
 };

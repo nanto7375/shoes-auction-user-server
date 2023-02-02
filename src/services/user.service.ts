ccb97c7020d8196297f261e0c64df3f0b37af888
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-02-03 01:19:44 +0900

    docs: 코드 리뷰 추가

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -15,6 +15,10 @@ export const isExistUser = async ( userId: string  , email: string  ): Promise<b
 };
 
 export const findPasswordAndRoleByUserId = async ({ userId }) => {
+  // ! userRepository에 findPasswordAndRoleByUserId는 생각해 보면 findOne의 특수한 형태임.
+  // ! userId 값으로 조회하는 건 지금뿐만 아니라 다른 경우에도 사용할 수 있을 것 같음!
+  // ! userRepository 내에 findPasswordAndRoleByUserId를 findOneByUserId로 바꾼 후 attribute 값은 따로 설정하지 않고
+  // ! 여기서 리턴 받는 user -> {password, role}로 변경해서 사용하면 이후에 findOneByUserId는 다른 곳에서도 재사용할 수 있음!
   const user = await UserRepository.findPasswordAndRoleByUserId({ userId });
   return user;
 };

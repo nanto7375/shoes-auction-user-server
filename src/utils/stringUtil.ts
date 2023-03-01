38ea28f8ef02cb21f86ecdf2ecdffccb9aafdfa4
seungwoo <jsu7375@gmail.com>
2023-03-02 01:38:19 +0900

    refactor: tempPassword 생성 부분 유틸 함수로 분리

--- src/utils/stringUtil.ts
+++ src/utils/stringUtil.ts
@@ -4,3 +4,5 @@ export const isEmail = ( email: string ): boolean => {
   const regexEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
   return !!email.match( regexEmail ); 
 };
+
+export const makeTempPassword = () => Array( 10 ).fill( null ).map( () => Math.round( Math.random() * 10 ) ).join( '' );
